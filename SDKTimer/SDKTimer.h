22e7dd9556123a13f2ebb551799e16e2f6fce029
Vitor Spessoto <spessoto97@icloud.com>
2021-03-10 17:28:37 -0300

    Initial Commit

---
+++ SDKTimer/SDKTimer.h
@@ -0,0 +1,18 @@
+//
+//  SDKTimer.h
+//  SDKTimer
+//
+//  Created by Vitor Spessoto on 10/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDKTimer.
+FOUNDATION_EXPORT double SDKTimerVersionNumber;
+
+//! Project version string for SDKTimer.
+FOUNDATION_EXPORT const unsigned char SDKTimerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKTimer/PublicHeader.h>
+
+
